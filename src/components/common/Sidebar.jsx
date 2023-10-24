4e4796c63b39839fef978dbb44810f5cab63dfd0
Gabriel Vargas <75455545+Odraga@users.noreply.github.com>
2023-10-23 22:35:27 -0400

    Update Sidebar.jsx

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -3,25 +3,40 @@ import { routes } from "../../Routes/routes";
 const Sidebar = () => {
   return (
     <header className="container-sm container-md container-lg">
-      <nav>
-        <a className="navbar-brand" href="#">
+      <nav className="my-3">
+        <a className="navbar-brand transition-color-1" href="#">
           GABRIEL DEV
         </a>
-        <ul className="navbar">
-          <li className="navbar-item">
-            <a href={routes.Home.path}>Home</a>
+
+        <div className="navbar d-sm-none">
+          <button className="button-toggle bgc-black rounded-5">ABRIR</button>
+        </div>
+
+        <ul className="navbar d-none d-md-flex">
+          <li className="navbar-item transition-color-1">
+            <a href={routes.Home.path} className="transition-color-1">
+              Home
+            </a>
+          </li>
+          <li className="navbar-item transition-color-1">
+            <a href={routes.Timer.path} className="transition-color-1">
+              Timer
+            </a>
+          </li>
+          <li className="navbar-item ">
+            <a href={routes.AboutUs.path} className="transition-color-1">
+              About
+            </a>
           </li>
           <li className="navbar-item">
-            <a href={routes.Timer.path}>Timer</a>
+            <a href={routes.AboutUs.path} className="transition-color-1">
+              Proyects
+            </a>
           </li>
-          <li className="navbar-item">
-            <a href={routes.AboutUs.path}>About</a>
-          </li>
-          <li className="navbar-item">
-            <a href={routes.AboutUs.path}>Proyects</a>
-          </li>
-          <li className="navbar-item">
-            <a href={routes.AboutUs.path}>Contact</a>
+          <li className="navbar-item ">
+            <a href={routes.AboutUs.path} className="transition-color-1">
+              Contact
+            </a>
           </li>
         </ul>
       </nav>
